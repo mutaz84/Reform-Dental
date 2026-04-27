2de6ee8256b0282271f1ecbd20119942280310ec
mutaz84 <110308235+mutaz84@users.noreply.github.com>
2026-04-27 13:24:23 -0500

    Disable equipment API caching

--- api/equipment/index.js
+++ api/equipment/index.js
@@ -67,6 +67,10 @@ function buildEquipmentColumnDefinitions(request, columns, body) {
 module.exports = async function (context, req) {
     const headers = {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type'
